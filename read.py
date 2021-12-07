99a1194258123d25fb551b41dda0ed16d30053c3
Bob Carpenter <carp@alias-i.com>
2021-12-06 21:52:44 -0500

    list to ndarray float32, int32

--- read.py
+++ read.py
@@ -5,7 +5,7 @@ from collections import deque
 from itertools import islice
 import numpy as np
 
-K = 4
+K = 10
 M = 4**K
 
 def shred(seq, K):
@@ -17,7 +17,7 @@ BASE_ID_CODE = {'A':0, 'C':1, 'G': 2, 'T':3}
 
 def base_to_code(b):
     return BASE_ID_CODE[b]
-        
+
 def kmer_to_id(kmer):
     id = 0;
     for c in kmer:
@@ -30,14 +30,17 @@ def valid_kmer(kmer):
             return False
     return True
 
+max_size = 400 * 1000 * 1000
+
 with open("data/GRCh38_latest_rna.fna") as fasta_file:
     parser = fastaparser.Reader(fasta_file, parse_method='quick')
     n = 0
-    data = list()
-    row_ind = list()
-    col_ind = list()
+    data = np.ndarray(max_size, dtype=np.float32)
+    row_ind = np.ndarray(max_size, dtype=np.int32)
+    col_ind = np.ndarray(max_size, dtype=np.int32)
+    pos = 1
     for seq in parser:
-        if (n % 100 == 0):
+        if (n % 1000 == 0):
             print("n = ", n)
         if "PREDICTED" in seq.header:
             continue
@@ -46,23 +49,27 @@ with open("data/GRCh38_latest_rna.fna") as fasta_file:
         kmer_counts = Counter(iter_filtered)
         total_count = sum(kmer_counts.values())
         for (kmer, count) in kmer_counts.items():
-            data.append(count / total_count)
-            row_ind.append(kmer_to_id(kmer))
-            col_ind.append(n)
+            data[pos] = np.float32(count / total_count)
+            row_ind[pos] = np.int32(kmer_to_id(kmer))
+            col_ind[pos] = np.int32(n)
+            pos = pos + 1
         n += 1
 
-print("building csr_matrix")
-xt = csr_matrix((data, (row_ind, col_ind)), shape = (M, n))
+print("resizing")
+data.resize(n)
+row_ind.resize(n)
+col_ind.resize(n)
 
+print("building csr_matrix")
+xt = csr_matrix((data, (row_ind, col_ind)), shape = (M, n), dtype=np.float32)
+
+
+# TESTING AFTER HERE
 rng = np.random.default_rng()
-theta = np.abs(rng.standard_normal(n))
-theta /= np.sum(theta)
+theta = np.abs(rng.standard_normal(n, dtype=np.float32))
+theta /= np.sum(theta)  # theta now a simplex
 
 y_hat = xt.dot(theta)
-print(y_hat)
-        
+# print(y_hat)
 
-    
-    
-
-
+print("sum(xt * theta) =", sum(y_hat), " [should be 1.0]")
