b067f3c77c0b125102a0faca1d0221ff7a8d5ab3
Bob Carpenter <carp@alias-i.com>
2021-12-12 20:35:17 -0500

    tested transcriptome reader

--- kmerexpr/test_transcriptome_reader.py
+++ kmerexpr/test_transcriptome_reader.py
@@ -1,31 +1,56 @@
-# import numpy as np
-# from scipy.sparse import load_npz
-
+import numpy as np
+import pytest
+from scipy.sparse import load_npz
 import transcriptome_reader as tr
 
-K = 2
-TRANSCRIPTOME_FILE='kmerexpr/test_data/test1.fsa'
-X_FILE = f"kmerexpr/test_data/x_{K}_csr.npz"
+ISO_FILE1 ='kmerexpr/test_data/test1.fsa'
+X_FILE1 = "kmerexpr/test_data/x1_csr.npz"
+K1 = 1
 
-def test_answer():
-    tr.transcriptome_to_x(K, TRANSCRIPTOME_FILE, X_FILE)
+def test_k1():
     assert 1 == 1
-    
-# print("loading x from file")
-# start_time = time.time()
-# x = load_npz(X_FILE)
-# M, N = x.get_shape()
-# print("M =", M, ";  N =", N)
-# end_time = time.time()
-# print("x loaded in time =", end_time - start_time, "seconds")
-# rng = np.random.default_rng()
-# theta = np.abs(rng.standard_normal(N, dtype=np.float32))
-# theta /= np.sum(theta)  # theta now an
-# y_hat = x.dot(theta)
-# start_time = time.time()
-# print("sum(x * theta) =", sum(y_hat), " [should be 1.0]")
-# end_time = time.time()
-# print("multiply time =", end_time - start_time, "seconds")
+    tr.transcriptome_to_x(K1, ISO_FILE1, X_FILE1)
+    x = load_npz(X_FILE1)
+    M, T = x.get_shape()
+    assert M == 4**1
+    assert T == 2
+    assert x.count_nonzero() == 8
+    assert x[0, 0] == 0.25
+    assert x[1, 0] == 0.25
+    assert x[2, 0] == 0.25
+    assert x[3, 0] == 0.25
+    assert x[0, 1] == pytest.approx(0.40, 1e-6)
+    assert x[1, 1] == pytest.approx(0.20, 1e-6)
+    assert x[2, 1] == pytest.approx(0.20, 1e-6)
+    assert x[3, 1] == pytest.approx(0.20, 1e-6)
+
+ISO_FILE2 ='kmerexpr/test_data/test2.fsa'
+X_FILE2 = "kmerexpr/test_data/x2_csr.npz"
+K2 = 2
+
+def test_k2():
+    tr.transcriptome_to_x(K2, ISO_FILE2, X_FILE2)
+    x = load_npz(X_FILE2)
+    M, T = x.get_shape()
+    assert M == 4**2
+    assert T == 3
+    assert x[0, 0] == pytest.approx(1.0 / 3.0, 1e-6)
+    assert x[1, 0] == pytest.approx(1.0 / 3.0, 1e-6)
+    assert x[5, 0] == pytest.approx(1.0 / 3.0, 1e-6)
+    assert x[6, 1] == pytest.approx(1.0 / 3.0, 1e-6)
+    assert x[10, 1] == pytest.approx(1.0 / 3.0, 1e-6)
+    assert x[15, 2] == 1.0
+
+def test_simplex():
+    tr.transcriptome_to_x(K2, ISO_FILE2, X_FILE2)
+    x = load_npz(X_FILE2)
+    x = load_npz(X_FILE2)
+    M, T = x.get_shape()
+    rng = np.random.default_rng()
+    theta = np.abs(rng.standard_normal(T, dtype=np.float32))
+    theta /= np.sum(theta)  # theta now simplex
+    y_hat = x.dot(theta)
+    assert sum(y_hat) == pytest.approx(1.0, 1e-5)
 
 
 
