56b026250fe9a51040cf23f89c66362e483867f7
Frederik <frederik.kunstner@gmail.com>
2022-11-29 13:30:08 -0800

    Add test to import each submodule in kmerexpr

---
+++ test/test_imports.py
@@ -0,0 +1,23 @@
+
+
+def test_imports():
+    import kmerexpr
+    from kmerexpr import benchmark_read_length
+    from kmerexpr import compare_optimizers
+    from kmerexpr import download_data
+    from kmerexpr import exp_grad_solver
+    from kmerexpr import fasta
+    from kmerexpr import frank_wolfe
+    from kmerexpr import multinomial_model
+    from kmerexpr import multinomial_simplex_model
+    from kmerexpr import normal_model
+    from kmerexpr import plot_kmer_density
+    from kmerexpr import plotting
+    from kmerexpr import rna_seq_reader
+    from kmerexpr import rob_plaay
+    from kmerexpr import run_single
+    from kmerexpr import sample_genome_data
+    from kmerexpr import simple_example
+    from kmerexpr import simulate_reads
+    from kmerexpr import transcriptome_reader
+    from kmerexpr import utils
